b5b44b36024d6c12dbd195454f01dcb7a49406ff
truc5dao <05lejardin@gmail.com>
2025-05-27 20:47:17 +0700

    commit for new homework 27/05/2025

--- app/src/main/java/com/cuatrucdao/k22411csampleproject/AdvancedProductManagementActivity.java
+++ app/src/main/java/com/cuatrucdao/k22411csampleproject/AdvancedProductManagementActivity.java
@@ -1,16 +1,26 @@
 package com.cuatrucdao.k22411csampleproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.cuatrucdao.adapters.ProductAdapter;
+import com.cuatrucdao.models.Customer;
 import com.cuatrucdao.models.ListProduct;
+import com.cuatrucdao.models.Product;
 
 public class AdvancedProductManagementActivity extends AppCompatActivity {
     ListView lvAdvancedProduct;
@@ -29,7 +39,44 @@ public class AdvancedProductManagementActivity extends AppCompatActivity {
         });
 
         addViews();
+        addEvents();
     }
+
+    private void addEvents() {
+        {
+//            lvAdvancedProduct.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                @Override
+//                public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+//                    Product p = adapter.getItem(i);
+//                    displayAdvancedProductDetailActivity(p);
+//                }
+//            });
+
+            lvAdvancedProduct.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                    Product p = adapter.getItem(i);
+                    if (p == null) {
+                        Toast.makeText(AdvancedProductManagementActivity.this, "Product is null!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    Toast.makeText(AdvancedProductManagementActivity.this, "Clicked: " + p.getName(), Toast.LENGTH_SHORT).show();
+                    displayAdvancedProductDetailActivity(p);
+                }
+            });
+
+        }
+    }
+
+
+    private void displayAdvancedProductDetailActivity(Product p) {
+        Intent intent = new Intent(AdvancedProductManagementActivity.this, AdvancedProductDetailActivity.class);
+        intent.putExtra("SELECTED_PRODUCT", p);
+        startActivity(intent);
+
+
+    }
+
 
     private void addViews() {
         lvAdvancedProduct = findViewById(R.id.lvAdvancedProduct);
@@ -40,5 +87,30 @@ public class AdvancedProductManagementActivity extends AppCompatActivity {
         listProduct = new ListProduct();
         listProduct.generate_sample_product_dataset();
         adapter.addAll(listProduct.getProducts());
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu_product, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_new_product) {
+            Toast.makeText(AdvancedProductManagementActivity.this,
+                    "Open the screen to add a new product",
+                    Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(AdvancedProductManagementActivity.this, AdvancedProductDetailActivity.class);
+            //startActivity(intent);
+            // dong goi va dat ma request code la 300
+            startActivityForResult(intent, 300);
+
+        } else if (item.getItemId() == R.id.menu_help) {
+            Toast.makeText(AdvancedProductManagementActivity.this,
+                    "Open website guiding instructions",
+                    Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
